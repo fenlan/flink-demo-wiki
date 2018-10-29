ad51f87db17de6b162696b5163dea9f7b95b650a
fenlan <1475307818@qq.com>
2018-10-29 23:52:28 +0800

    维基百科编辑日志

--- src/main/java/com/fenlan/flink/wiki/WikipediaAnalysis.java
+++ src/main/java/com/fenlan/flink/wiki/WikipediaAnalysis.java
@@ -1,6 +1,7 @@
 package com.fenlan.flink.wiki;
 
 import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
@@ -16,19 +17,16 @@ public class WikipediaAnalysis {
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
 
-        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits.keyBy(WikipediaEditEvent::getUser);
-
-        DataStream<Tuple2<String, Long>> result = keyedEdits
+        DataStream<Tuple2<String, Long>> result = edits.map(new MapFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+            @Override
+            public Tuple2<String, Long> map(WikipediaEditEvent event) throws Exception {
+                return new Tuple2<>(event.getUser(), (long)event.getByteDiff());
+            }
+        })
+                .keyBy(stringLongTuple2 -> stringLongTuple2.f0)
                 .timeWindow(Time.seconds(5))
-                .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
-                    @Override
-                    public Tuple2<String, Long> fold(Tuple2<String, Long> stringLongTuple2, WikipediaEditEvent o) {
-                        stringLongTuple2.f0 = o.getUser();
-                        stringLongTuple2.f1 += o.getByteDiff();
-                        return stringLongTuple2;
-                    }
-                });
-        result.print();
+                .reduce((t1, t2) -> new Tuple2<>(t1.f0, t1.f1+t2.f1));
+        result.print().setParallelism(1);
         see.execute();
     }
 }
