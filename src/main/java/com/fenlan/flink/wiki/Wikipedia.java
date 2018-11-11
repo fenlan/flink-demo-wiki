984b6aeb1aacce94bee8ae60366d03e47a921761
fenlan <1475307818@qq.com>
2018-11-11 16:18:36 +0800

    wiki edit detail

---
+++ src/main/java/com/fenlan/flink/wiki/Wikipedia.java
@@ -0,0 +1,20 @@
+package com.fenlan.flink.wiki;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+
+public class Wikipedia {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
+
+        edits.print();
+        see.execute();
+    }
+}
