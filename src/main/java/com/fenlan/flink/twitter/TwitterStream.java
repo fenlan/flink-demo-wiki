ed3d71b44c1c526db045d6f94d0415ba10edff03
fenlan <1475307818@qq.com>
2018-11-12 13:52:35 +0800

    twitter数据源

---
+++ src/main/java/com/fenlan/flink/twitter/TwitterStream.java
@@ -0,0 +1,89 @@
+package com.fenlan.flink.twitter;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+
+import java.util.Arrays;
+
+public class TwitterStream {
+
+    public static void main(String[] args) {
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey("sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe");
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+        String channelName = "pubnub-twitter";
+
+        pubnub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+
+
+                if (status.getCategory() == PNStatusCategory.PNUnexpectedDisconnectCategory) {
+                    // This event happens when radio / connectivity is lost
+                }
+
+                else if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+
+                    // Connect event. You can do stuff like publish, and know you'll get it.
+                    // Or just use the connected event to confirm you are subscribed for
+                    // UI / internal notifications, etc
+
+                    if (status.getCategory() == PNStatusCategory.PNConnectedCategory){
+                    }
+                }
+                else if (status.getCategory() == PNStatusCategory.PNReconnectedCategory) {
+
+                    // Happens as part of our regular operation. This event happens when
+                    // radio / connectivity is lost, then regained.
+                }
+                else if (status.getCategory() == PNStatusCategory.PNDecryptionErrorCategory) {
+
+                    // Handle messsage decryption error. Probably client configured to
+                    // encrypt messages and on live data feed it received plain text.
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                // Handle new message stored in message.message
+                if (message.getChannel() != null) {
+                    // Message has been received on channel group stored in
+                    // message.getChannel()
+                }
+                else {
+                    // Message has been received on channel stored in
+                    // message.getSubscription()
+                }
+
+                JsonElement receivedMessageObject = message.getMessage();
+                System.out.println("Received message content: " + receivedMessageObject.toString());
+                // extract desired parts of the payload, using Gson
+
+            /*
+                log the following items with your favorite logger
+                    - message.getMessage()
+                    - message.getSubscription()
+                    - message.getTimetoken()
+            */
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+
+        pubnub.subscribe().channels(Arrays.asList(channelName)).execute();
+    }
+}
